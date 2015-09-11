a538267e35c504f49e93c7e659e1fd04c24abe90
Zack Pollard <iam@zackpollard.pro>
2015-09-12 00:08:23 +0100

    Time to mix it up, changing how it all works

---
+++ api/src/main/java/org/telegram/botapi/api/TelegramBot.java
@@ -0,0 +1,24 @@
+package org.telegram.botapi.api;
+
+import lombok.Getter;
+import org.telegram.botapi.api.managers.FileManager;
+
+import java.util.logging.Level;
+
+/**
+ * @author Zack Pollard
+ */
+public final class TelegramBot {
+
+    /**
+     * Use this method to get a new TelegramBot instance with the selected auth token.
+     *
+     * @param authToken The bots auth token.
+     * @return A new TelegramBot instance to base your bot around.
+     */
+
+    public static TelegramBot login(String authToken) {
+
+
+    }
+}
