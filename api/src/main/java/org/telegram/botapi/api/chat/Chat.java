7c04bfc4a5411ae32185484c278ae6c2b4f20b87
Zack Pollard <iam@zackpollard.pro>
2015-09-13 02:07:25 +0100

    Changed Chat#sendMessage(String) and Chat#sendMessage(SendableMessage) to include a TelegramBot argument in order to support multiple bots in one program

--- api/src/main/java/org/telegram/botapi/api/chat/Chat.java
+++ api/src/main/java/org/telegram/botapi/api/chat/Chat.java
@@ -1,5 +1,6 @@
 package org.telegram.botapi.api.chat;
 
+import org.telegram.botapi.api.TelegramBot;
 import org.telegram.botapi.api.chat.message.Message;
 import org.telegram.botapi.api.chat.message.send.SendableMessage;
 import org.telegram.botapi.api.chat.message.send.SendableTextMessage;
@@ -11,10 +12,10 @@ public interface Chat {
 
     int getId();
 
-    default Message sendMessage(String message) {
+    default Message sendMessage(String message, TelegramBot telegramBot) {
 
-        return this.sendMessage(SendableTextMessage.builder().message(message).build());
+        return this.sendMessage(SendableTextMessage.builder().message(message).build(), telegramBot);
     }
 
-    Message sendMessage(SendableMessage message);
+    Message sendMessage(SendableMessage message, TelegramBot telegramBot);
 }
