d93d60f5c00201d1f346cbb59eaf50f414916780
Zack Pollard <iam@zackpollard.pro>
2015-09-13 02:03:42 +0100

    Added Audio#getPerformer() and Audio#getTitle() due to bot API updates.

--- api/src/main/java/org/telegram/botapi/api/chat/message/content/type/Audio.java
+++ api/src/main/java/org/telegram/botapi/api/chat/message/content/type/Audio.java
@@ -4,4 +4,7 @@ package org.telegram.botapi.api.chat.message.content.type;
  * @author Zack Pollard
  */
 public interface Audio extends DurationableFile, Mimetypeable {
+
+	String getPerformer();
+	String getTitle();
 }
