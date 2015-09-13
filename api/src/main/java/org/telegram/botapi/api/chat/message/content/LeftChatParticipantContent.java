fe140c589722cf4264e1bcade7a59730b32a3034
Zack Pollard <iam@zackpollard.pro>
2015-09-13 02:02:57 +0100

    Added LeftChatParticipantContent message type.

---
+++ api/src/main/java/org/telegram/botapi/api/chat/message/content/LeftChatParticipantContent.java
@@ -0,0 +1,22 @@
+package org.telegram.botapi.api.chat.message.content;
+
+import org.telegram.botapi.api.user.User;
+
+/**
+ * @author Zack Pollard
+ */
+public interface LeftChatParticipantContent extends Content {
+
+    /**
+     * Gets the participant who left the chat
+     *
+     * @return The previous participant, or null if there was no participant
+     */
+    User getContent();
+
+    @Override
+    default ContentType getType() {
+
+        return ContentType.LEFT_CHAT_PARTICIPANT;
+    }
+}
