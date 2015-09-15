443af78718add2cf441a3a61b1d9d1e466ebd7df
Zack Pollard <iam@zackpollard.pro>
2015-09-15 01:08:44 +0100

    Move everything out of the api module as it is unneeded now

--- src/main/java/org/telegram/botapi/api/event/Cancellable.java
+++ src/main/java/org/telegram/botapi/api/event/Cancellable.java
@@ -7,7 +7,7 @@ public interface Cancellable {
 
 	/**
 	 * Gets the cancellation state of this event. A cancelled event will not
-	 * be executed in the server, but will still pass to other plugins
+	 * be executed in the API, but will still pass to other registered listeners
 	 *
 	 * @return true if this event is cancelled
 	 */
@@ -15,7 +15,7 @@ public interface Cancellable {
 
 	/**
 	 * Sets the cancellation state of this event. A cancelled event will not
-	 * be executed in the server, but will still pass to other plugins.
+	 * be executed in the API, but will still pass to other registered listeners.
 	 *
 	 * @param cancel true if you wish to cancel this event
 	 */
