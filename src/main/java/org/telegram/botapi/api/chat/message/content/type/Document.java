cce14b2af61289d52ba79c8c3c643af6d518c6b5
Zack Pollard <iam@zackpollard.pro>
2015-09-19 01:42:55 +0100

    Added Document#getFileName()

--- src/main/java/org/telegram/botapi/api/chat/message/content/type/Document.java
+++ src/main/java/org/telegram/botapi/api/chat/message/content/type/Document.java
@@ -4,4 +4,6 @@ package org.telegram.botapi.api.chat.message.content.type;
  * @author Zack Pollard
  */
 public interface Document extends File, Mimetypeable, Thumbnailable {
+
+    String getFileName();
 }
