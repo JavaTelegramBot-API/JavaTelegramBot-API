e470b60c1f6faec8e495d95c759ace67bf557074
Zack Pollard <iam@zackpollard.pro>
2015-09-19 02:01:58 +0100

    Removed debug messages, whoops again...

--- src/main/java/org/telegram/botapi/api/chat/message/content/type/File.java
+++ src/main/java/org/telegram/botapi/api/chat/message/content/type/File.java
@@ -65,8 +65,6 @@ public interface File {
             }
         }
 
-        System.out.println("Downloaded file to " + downloadLocation.getAbsolutePath());
-
         return downloadLocation;
 	}
 }
