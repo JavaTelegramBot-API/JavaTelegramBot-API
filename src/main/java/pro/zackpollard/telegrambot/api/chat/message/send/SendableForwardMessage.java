33478310850375fc3e04b3a3aa9219df02e94535
Zack Pollard <iam@zackpollard.pro>
2015-12-15 02:21:33 +0000

    Allow forwarding messages by ID as well as by a Message object, resolves #13

--- src/main/java/pro/zackpollard/telegrambot/api/chat/message/send/SendableForwardMessage.java
+++ src/main/java/pro/zackpollard/telegrambot/api/chat/message/send/SendableForwardMessage.java
@@ -1,6 +1,5 @@
 package pro.zackpollard.telegrambot.api.chat.message.send;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -11,15 +10,54 @@ import pro.zackpollard.telegrambot.api.chat.message.Message;
  */
 
 @RequiredArgsConstructor
-@Builder
 public class SendableForwardMessage implements SendableMessage {
 
 	@NonNull
 	@Getter
-	private final Message forwardedMessage;
+	private final Integer forwardedMessage;
 
-	@Override
+    SendableForwardMessage(int forwardedMessage) {
+
+        this.forwardedMessage = forwardedMessage;
+    }
+
+    SendableForwardMessage(Message forwardedMessage) {
+
+        this(forwardedMessage != null ? forwardedMessage.getMessageId() : null);
+    }
+
+    public static SendableForwardMessageBuilder builder() {
+        return new SendableForwardMessageBuilder();
+    }
+
+    @Override
 	public MessageType getType() {
 		return MessageType.FORWARDED;
 	}
+
+    public static class SendableForwardMessageBuilder {
+
+        private int forwardedMessage;
+
+        SendableForwardMessageBuilder() {
+        }
+
+        public SendableForwardMessage.SendableForwardMessageBuilder forwardedMessage(Message forwardedMessage) {
+            this.forwardedMessage = forwardedMessage != null ? forwardedMessage.getMessageId() : null;
+            return this;
+        }
+
+        public SendableForwardMessage.SendableForwardMessageBuilder forwardedMessage(Integer forwardedMessage) {
+            this.forwardedMessage = forwardedMessage;
+            return this;
+        }
+
+        public SendableForwardMessage build() {
+            return new SendableForwardMessage(forwardedMessage);
+        }
+
+        public String toString() {
+            return "pro.zackpollard.telegrambot.api.chat.message.send.SendableForwardMessage.SendableForwardMessageBuilder(forwardedMessage=" + this.forwardedMessage + ")";
+        }
+    }
 }
