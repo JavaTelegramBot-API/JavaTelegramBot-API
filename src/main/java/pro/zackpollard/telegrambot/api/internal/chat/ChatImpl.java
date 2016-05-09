dfe12bfbffa899242af30ee4969736a3bc5a7fb0
Zack Pollard <zackpollard@ymail.com>
2016-05-09 05:18:51 +0100

    Push a fix for when forward_from_chat is not populated.

--- src/main/java/pro/zackpollard/telegrambot/api/internal/chat/ChatImpl.java
+++ src/main/java/pro/zackpollard/telegrambot/api/internal/chat/ChatImpl.java
@@ -11,20 +11,23 @@ public class ChatImpl {
 
     public static Chat createChat(JSONObject jsonObject, TelegramBot telegramBot) {
 
-        String chatType = jsonObject.getString("type");
+        if(jsonObject != null) {
 
-        switch (chatType) {
+            String chatType = jsonObject.getString("type");
 
-            case "private":
-                return IndividualChatImpl.createIndividualChat(jsonObject, telegramBot);
-            case "group":
-                return GroupChatImpl.createGroupChat(jsonObject, telegramBot);
-            case "channel":
-                return ChannelChatImpl.createChannelChat(jsonObject, telegramBot);
-            case "supergroup":
-                return SuperGroupChatImpl.createSuperGroupChat(jsonObject, telegramBot);
-            default:
-                System.err.println("An invalid chat type was provided when creating a chat object. Chat type " + chatType + " was provided. Report this to @zackpollard.");
+            switch (chatType) {
+
+                case "private":
+                    return IndividualChatImpl.createIndividualChat(jsonObject, telegramBot);
+                case "group":
+                    return GroupChatImpl.createGroupChat(jsonObject, telegramBot);
+                case "channel":
+                    return ChannelChatImpl.createChannelChat(jsonObject, telegramBot);
+                case "supergroup":
+                    return SuperGroupChatImpl.createSuperGroupChat(jsonObject, telegramBot);
+                default:
+                    System.err.println("An invalid chat type was provided when creating a chat object. Chat type " + chatType + " was provided. Report this to @zackpollard.");
+            }
         }
 
         return null;
