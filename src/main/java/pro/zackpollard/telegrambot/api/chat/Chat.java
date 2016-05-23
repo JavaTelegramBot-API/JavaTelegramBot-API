244fe68ef2983b7fc6c9f6e5947c4ce0384e472c
Zack Pollard <zackpollard@ymail.com>
2016-05-23 01:48:24 +0100

    Implemented Chat#getChatAdministrators()

--- src/main/java/pro/zackpollard/telegrambot/api/chat/Chat.java
+++ src/main/java/pro/zackpollard/telegrambot/api/chat/Chat.java
@@ -4,12 +4,17 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import com.mashape.unirest.request.body.MultipartBody;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import pro.zackpollard.telegrambot.api.TelegramBot;
 import pro.zackpollard.telegrambot.api.chat.message.Message;
 import pro.zackpollard.telegrambot.api.chat.message.send.SendableMessage;
 import pro.zackpollard.telegrambot.api.chat.message.send.SendableTextMessage;
+import pro.zackpollard.telegrambot.api.internal.chat.ChatMemberImpl;
 import pro.zackpollard.telegrambot.api.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static pro.zackpollard.telegrambot.api.utils.Utils.processResponse;
 
@@ -45,6 +50,37 @@ public interface Chat {
             if (jsonResponse != null && Utils.checkResponseStatus(jsonResponse)) {
 
                 return jsonResponse.getInt("result");
+            }
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    default List<ChatMember> getChatAdministrators() {
+
+        try {
+
+            MultipartBody request = Unirest.post(getBotInstance().getBotAPIUrl() + "getChatAdministrators")
+                    .field("chat_id", getId(), "application/json");
+            HttpResponse<String> response = request.asString();
+            JSONObject jsonResponse = processResponse(response);
+
+            if (jsonResponse != null && Utils.checkResponseStatus(jsonResponse)) {
+
+                JSONArray jsonArray = jsonResponse.getJSONArray("result");
+
+                List<ChatMember> chatAdmins = new ArrayList<>();
+
+                for(int i = 0; i < jsonArray.length(); ++i) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    chatAdmins.add(ChatMemberImpl.createChatMember(jsonObject));
+                }
+
+                return chatAdmins;
             }
         } catch (UnirestException e) {
             e.printStackTrace();
