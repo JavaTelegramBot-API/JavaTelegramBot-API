6cd2aabaf4272404331fa99b972c1af227a4a474
Zack Pollard <zackpollard@ymail.com>
2016-08-15 09:28:59 +0100

    Utils Documentation

--- src/main/java/pro/zackpollard/telegrambot/api/utils/Utils.java
+++ src/main/java/pro/zackpollard/telegrambot/api/utils/Utils.java
@@ -19,6 +19,13 @@ import java.util.Random;
  */
 public class Utils {
 
+    /**
+     * Generates a random alphanumeric String of the length specified
+     *
+     * @param length The required length of the String
+     *
+     * @return A random alphanumeric String
+     */
     public static String generateRandomString(int length) {
 
         char[] chars = "abcdefghijklmnopqrstuvwxyz1234567890".toCharArray();
@@ -32,6 +39,13 @@ public class Utils {
         return sb.toString();
     }
 
+    /**
+     * This does some generic processing on HttpResponse objects returned by the Telegram Bot API
+     *
+     * @param response The HttpResponse object returned by Unirest from the Telegram Bot API call
+     *
+     * @return A JSONObject containing the processed response or null if something was wrong with the response
+     */
     public static JSONObject processResponse(HttpResponse<String> response) {
 
         if (response != null) {
@@ -68,6 +82,12 @@ public class Utils {
         return null;
     }
 
+    /**
+     * This does generic processing of ReplyingOptions objects when sending a request to the API
+     *
+     * @param multipartBody     The MultipartBody that the ReplyingOptions content should be appended to
+     * @param replyingOptions   The ReplyingOptions that were used in this request
+     */
     public static void processReplyContent(MultipartBody multipartBody, ReplyingOptions replyingOptions) {
 
         if (replyingOptions.getReplyTo() != 0)
@@ -92,11 +112,24 @@ public class Utils {
         }
     }
 
+    /**
+     * This does generic processing of NotificationOptions objects when sending a request to the API
+     *
+     * @param multipartBody         The MultipartBody that the NotificationOptions content should be appended to
+     * @param notificationOptions   The NotificationOptions that were used in this request
+     */
     public static void processNotificationContent(MultipartBody multipartBody, NotificationOptions notificationOptions) {
 
         multipartBody.field("disable_notification", notificationOptions.isDisableNotification());
     }
 
+    /**
+     * This does generic processing of processed JSON objects to ensure that the API returned ok: true
+     *
+     * @param jsonResponse The JSONObject that you want to check
+     *
+     * @return True if the JSONObject contained ok: true, otherwise False
+     */
     public static boolean checkResponseStatus(JSONObject jsonResponse) {
 
         if (jsonResponse != null) {
@@ -116,16 +149,33 @@ public class Utils {
         return false;
     }
 
+    /**
+     * Generic method for ensuring an object isn't null, if it is null a NullPointerException is thrown
+     *
+     * @param object The object you want to check isn't null
+     */
     public static void validateNotNull(Object object) {
         validateNotNull(object, "");
     }
 
+    /**
+     * Generic method for ensuring an object isn't null, if it is null a NullPointerException is thrown
+     *
+     * @param object    The object you want to check isn't null
+     * @param message   The message you want the NullPointerException to have in the case that the object is null
+     */
     public static void validateNotNull(Object object, String message) {
         if (object == null) {
             throw new NullPointerException(message);
         }
     }
 
+    /**
+     * Generic method for ensuring an array of objects aren't null, if any object is null a NullPointerException is
+     * thrown
+     *
+     * @param objects The objects that you want to ensure aren't null
+     */
     public static void validateNotNull(Object... objects) {
         for (Object o : objects) {
             validateNotNull(o);
