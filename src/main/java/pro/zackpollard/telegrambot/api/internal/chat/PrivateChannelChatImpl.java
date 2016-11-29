6494c1b3ec749f8d8b79d331f8cb6babf0c6bc44
Mohammed El Khoudary <m.elkhoudary@gmail.com>
2016-11-29 20:43:29 +0200

    Separated Channel Chat from Private Channel Chat, first one is identified by username, second one is identified by chat id

---
+++ src/main/java/pro/zackpollard/telegrambot/api/internal/chat/PrivateChannelChatImpl.java
@@ -0,0 +1,68 @@
+package pro.zackpollard.telegrambot.api.internal.chat;
+
+import org.json.JSONObject;
+import pro.zackpollard.telegrambot.api.TelegramBot;
+import pro.zackpollard.telegrambot.api.chat.ChannelChat;
+import pro.zackpollard.telegrambot.api.chat.message.Message;
+import pro.zackpollard.telegrambot.api.chat.message.send.SendableMessage;
+
+/**
+ * @author Zack Pollard
+ */
+public class ChannelChatImpl implements ChannelChat {
+
+    private final String username;
+    private final String title;
+
+    private final TelegramBot telegramBot;
+
+    private ChannelChatImpl(JSONObject jsonObject, TelegramBot telegramBot) {
+
+        this.username = "@" + jsonObject.optString("username");
+        this.title = jsonObject.getString("title");
+        this.telegramBot = telegramBot;
+    }
+
+    private ChannelChatImpl(String username, TelegramBot telegramBot) {
+
+        this.username = username;
+        this.title = null;
+        this.telegramBot = telegramBot;
+    }
+
+    public static ChannelChat createChannelChat(JSONObject jsonObject, TelegramBot telegramBot) {
+
+        return new ChannelChatImpl(jsonObject, telegramBot);
+    }
+
+    public static ChannelChat createChannelChat(String username, TelegramBot telegramBot) {
+
+        return new ChannelChatImpl(username, telegramBot);
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getName() {
+        return title;
+    }
+
+    @Override
+    public TelegramBot getBotInstance() {
+        return telegramBot;
+    }
+
+    @Override
+    public String getId() {
+        return username;
+    }
+
+    @Override
+    public Message sendMessage(SendableMessage message) {
+
+        return telegramBot.sendMessage(this, message);
+    }
+}
