0be8ba8c18d08279e3e97037ab57ab8e31fea2f4
Zack Pollard <zackpollard@ymail.com>
2017-01-30 09:02:22 +0000

    Add a CommandMessageReceivedEvent#isBotMentioned() to make it easier to differentiate between global commands and direct commands in group chats

--- core/src/main/java/pro/zackpollard/telegrambot/api/event/chat/message/CommandMessageReceivedEvent.java
+++ core/src/main/java/pro/zackpollard/telegrambot/api/event/chat/message/CommandMessageReceivedEvent.java
@@ -12,12 +12,15 @@ public class CommandMessageReceivedEvent extends TextMessageReceivedEvent {
     private final String[] args;
     private final String argsString;
     private final String command;
+    private final boolean botMentioned;
 
     public CommandMessageReceivedEvent(Message message) {
 
         super(message);
 
-        this.command = getContent().getContent().substring(1).split(" ")[0].split("@")[0];
+        String[] commandSections = getContent().getContent().substring(1).split(" ")[0].split("@");
+        this.command = commandSections[0];
+        botMentioned = commandSections.length >= 1;
 
         int argsStart = getContent().getContent().indexOf(" ");
 
@@ -57,5 +60,15 @@ public class CommandMessageReceivedEvent extends TextMessageReceivedEvent {
      */
     public String getCommand() {
         return this.command;
+    }
+
+    /**
+     * Gets whether the bot was mentioned in this command, or whether it was just the command that was sent.
+     *
+     * @return  True if the command was of form `/command@botname args` where botname is the name of this bot,
+     *          false if the command was of form `/command args`
+     */
+    public boolean isBotMentioned() {
+        return this.botMentioned;
     }
 }
