2a98efcc36ec273a6cf08c96224dc9c747e80be1
Jayasree Kulothungan <jy525824@dal.ca>
2022-03-25 00:42:04 -0300

    Pus Down Method - Refactoring

--- core/src/main/java/pro/zackpollard/telegrambot/api/chat/SuperGroupChat.java
+++ core/src/main/java/pro/zackpollard/telegrambot/api/chat/SuperGroupChat.java
@@ -18,15 +18,6 @@ public interface SuperGroupChat extends GroupChat {
     String getUsername();
 
     /**
-     * This method will kick and ban the User with the specified User ID from the chat if they are currently in it
-     *
-     * @param userId The User ID of the User you want to kick from the chat
-     *
-     * @return True if the user was kicked, otherwise False
-     */
-    boolean kickChatMember(int userId);
-
-    /**
      * This method will unban a user that was previously banned from the chat
      *
      * @param userId The ID of the User you want to unban
